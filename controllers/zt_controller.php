a5d7e16c7bd20dcd50e83aadd50b8fbeef2261cf
jon.huang <sakura.speed@gmail.com>
2014-08-14 22:57:09 +0800

    增加新人科普专题

---
+++ controllers/zt_controller.php
@@ -0,0 +1,14 @@
+<?php
+//H5专题页面
+class ZtController extends AppController {
+
+	var $name = 'Zt';
+	var $components = array('Pagination');
+	var $cacheAction = array();
+
+	//新人引导
+	function guide(){
+
+	}
+}
+?>
