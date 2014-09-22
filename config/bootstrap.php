bd4c21497895ec0472ff5333a598e3304066f250
jon.huang <sakura.speed@gmail.com>
2014-09-22 20:55:26 +0800

    移动端导航栏调用按钮

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,3 +1,13 @@
 <?php
 
+//在移动端导航栏调用自定义按钮
+function navButton($value, $url){
+
+	if(!$value || !$url)return;
+	if(getBrowser() == 'android'){
+		return "<script type='text/javascript'>WebDetailActivity.setTitleAndButton('{$_POST['page_title']}', '{$value}', '{$url}')</script>";
+	}else{
+		return "<input type='hidden' id='setting_title' value='{$value}'><input type='hidden' id='setting_url' value='{$url}'/>";
+	}
+}
 ?>
