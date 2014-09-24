79dfbb4934888fca7162de5fbeefc4eac6f5ad51
jon.huang <sakura.speed@gmail.com>
2014-09-24 23:37:11 +0800

    调整android导航button触发js

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -5,7 +5,7 @@ function navButton($value, $url){
 
 	if(!$value || !$url)return;
 	if(getBrowser() == 'android'){
-		return "<script type='text/javascript'>WebDetailActivity.setTitleAndButton('{$_POST['page_title']}', '{$value}', '{$url}')</script>";
+		return "<script type='text/javascript'>WebDetailActivity.setTitleAndButton('', '{$value}', '{$url}')</script>";
 	}else{
 		return "<input type='hidden' id='setting_title' value='{$value}'><input type='hidden' id='setting_url' value='{$url}'/>";
 	}
